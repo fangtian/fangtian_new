6503004d85cc380559a7109be3d982ce8e3d71b5
huangdong-fangtian <huangdong@fangtian.me>
2016-12-23 10:02:07 +0800

    footer css

--- _/components/php/header.php
+++ _/components/php/header.php
@@ -10,13 +10,13 @@
 	        <span class="icon-bar"></span>
 	        <span class="icon-bar"></span>
 	      </button>
-	      <a class="navbar-brand" href="index.php">方田教育</a>
+	      <a class="navbar-brand" href="index.html">方田教育</a>
 	    </div>
 
 	    <!-- Collect the nav links, forms, and other content for toggling -->
 	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 	      <ul class="nav navbar-nav">
-	        <li><a href="index.php">首页</a></li>
+	        <li><a href="index.html">首页</a></li>
 	        <li><a href="course.php">选课中心</a></li>
 	        <li><a href="interaction.php">交流中心</a></li>
 	        <li><a href="about.php">关于我们</a></li>
